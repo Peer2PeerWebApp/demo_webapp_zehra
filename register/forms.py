79ce11fe8b88044804f41f32c4767658c4f4f6dd
Zehra Abacioglu <zehra@nyu.edu>
2019-10-31 16:36:19 -0400

    login/register/logout working now

--- register/forms.py
+++ register/forms.py
@@ -5,8 +5,11 @@ from .models import CustomUser
 
 
 class SignUpForm(UserCreationForm):
-    full_name = forms.CharField(max_length=100, help_text='Required. 100 charaters of fewer.')
+    #full_name = forms.CharField(max_length=100, help_text='Required. 100 charaters of fewer.')
+    is_tutee = forms.BooleanField(help_text='Select if you want to be a tutee.')
+    is_tutor = forms.BooleanField(help_text='Select if you want to be a tutor.')
 
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('full_name',)
+        #fields = UserCreationForm.Meta.fields + ('full_name',)
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'is_tutee', 'is_tutor']
--- register/models.py
+++ register/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    full_name = models.CharField(max_length=100, blank=False)
+    #full_name = models.CharField(max_length=100, blank=False)
     #age = models.PositiveIntegerField(null=True, blank=True)
     is_tutee = models.BooleanField(default=False)
     is_tutor = models.BooleanField(default=False)
