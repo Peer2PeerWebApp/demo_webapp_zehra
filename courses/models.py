8d9ad1baa41defae0563344ad7c23d17783087d7
Zehra Abacioglu <zehra@nyu.edu>
2019-11-16 17:39:01 -0500

    courses

--- courses/models.py
+++ courses/models.py
@@ -4,12 +4,20 @@ from register.models import CustomUser
 
 # Create your models here.
 
+class Department(models.Model):
+    department_code = models.CharField(max_length=10, null = False)    # e.g. CSCI
+    department_name = models.CharField(max_length=100, null = False)   # e.g. Computer Science
+
 
 class Courses(models.Model):
     course_name = models.CharField(max_length=100, null = False)
+    course_number = models.CharField(max_length=100, null = False)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    course_description = models.CharField(max_length=1000, blank=True)
     object = models.Manager()
     #class Meta:
     #    app_label = 'courses'
+
 
 
 class TutorTeachesCourse(models.Model):
@@ -18,3 +26,4 @@ class TutorTeachesCourse(models.Model):
         on_delete=models.CASCADE, 
         limit_choices_to=models.Q(is_status=True))
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    object = models.Manager()
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Courses
+from .models import Courses, TutorTeachesCourse
 
 # Create your views here.
 
@@ -10,3 +10,11 @@ def course_list(request):
         'course_list': course_list
     }  
     return render(request, 'course_list.html', context)
+
+
+def edit_tutor_courses(request):
+    course_list = Courses.object.all()
+    context = {
+        'course_list': course_list
+    }  
+    return render(request, 'edit_courses.html', context)
